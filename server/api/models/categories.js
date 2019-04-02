bd6359a471cc677b2ff93e53b8f26dcb6ef03127
ahmed hassan <eng.ahmedhassan.eng@gmail.com>
2019-04-02 20:00:27 +0400

    Revert "add to public repo"
    This reverts commit 2307f7bbdf3d0d4ca029afda3cba539f3fece597.

--- server/api/models/categories.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-var categorySchema = new mongoose.Schema({
-    title: String
-});
-
-module.exports = mongoose.model('categories', categorySchema);
--- server/api/routes/product.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ctrlProduct = require('../controllers/product');
-
-router.post('/products', ctrlProduct.new );
-router.get('/products', ctrlProduct.index );
-
-module.exports = router;
